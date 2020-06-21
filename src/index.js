69641ad86040566cfef46703bd7c71af661f4829
Dante Vella <dante.vella@gmail.com>
2020-06-21 12:26:05 -0400

    changed initialize

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-global.initialize = () => console.log('hey I was called');
+global.geotab.addin.index.initialize = () => console.log('hey I was called');
 
 ReactDOM.render(
   <React.StrictMode>
