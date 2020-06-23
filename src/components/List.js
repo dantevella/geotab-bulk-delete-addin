8b442a398f09e8d01f871385a0c67b8ff32ca183
Dante Vella <dante.vella@gmail.com>
2020-06-23 15:17:31 -0400

    Added button to perform delete from added files

--- src/components/List.js
+++ src/components/List.js
@@ -2,18 +2,17 @@ import React from "react";
 import handleDelete from "../utils/removeMulticalls";
 
 const List = (props) => {
-  //handleDelete(api, [id to delete], groups)
   const { groups, api } = props;
   if (!groups || groups.length === 0) return <p>No groups, sorry</p>;
   return (
     <ul>
       <h2 className="list-head">Available Groups to Delete</h2>
-      {groups.map((groups) => {
+      {groups.map((group) => {
         return (
-          <button type="button" key={groups.id} className="delete-buttons">
+          <button type="button" key={group.id} onClick={()=>handleDelete(api, [group.id], groups)}className="delete-buttons">
             Delete
-            <span className="repo-text">{" " + groups.name} </span>
-            <span className="repo-description">{groups.description}</span>
+            <span className="repo-text">{" " + group.name} </span>
+            <span className="repo-description">{group.description}</span>
           </button>
         );
       })}
